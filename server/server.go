02752c863131f5860549479a52233b075bac3e89
Kazuhiro Oinuma <oinume@gmail.com>
2018-10-10 10:09:09 +0900

    goimports

--- server/server.go
+++ server/server.go
@@ -3,34 +3,34 @@ package server
 import (
 	"context"
 
-	"github.com/oinume/grpc-sample/proto-gen/go/proto/api/v1"
 	protobuf_empty "github.com/golang/protobuf/ptypes/empty"
+	"github.com/oinume/grpc-sample/proto-gen/go/proto/api/v1"
 )
 
-type UsersServer struct {}
+type UsersServer struct{}
 
 func (s *UsersServer) ListUsers(ctx context.Context, in *api_v1.ListUsersRequest) (*api_v1.ListUsersResponse, error) {
 	return &api_v1.ListUsersResponse{
 		Users: []*api_v1.User{
-			{Id: "1", Name:"oinume", RealName: "kazuhiro oinuma"},
-			{Id: "2", Name:"akuwano", RealName: "Akihiro Kuwano"},
-			{Id: "3", Name:"oranie", RealName: "Takashi Narita"},
+			{Id: "1", Name: "oinume", RealName: "kazuhiro oinuma"},
+			{Id: "2", Name: "akuwano", RealName: "Akihiro Kuwano"},
+			{Id: "3", Name: "oranie", RealName: "Takashi Narita"},
 		},
 	}, nil
 }
 
 func (s *UsersServer) GetUser(ctx context.Context, in *api_v1.GetUserRequest) (*api_v1.User, error) {
 	return &api_v1.User{
-		Id: in.GetId(),
-		Name: "oinume",
+		Id:       in.GetId(),
+		Name:     "oinume",
 		RealName: "Kazuhiro Oinuma",
 	}, nil
 }
 
 func (s *UsersServer) CreateUser(ctx context.Context, in *api_v1.CreateUserRequest) (*api_v1.User, error) {
 	return &api_v1.User{
-		Id: "12345",
-		Name: in.GetName(),
+		Id:       "12345",
+		Name:     in.GetName(),
 		RealName: in.GetRealName(),
 	}, nil
 }
